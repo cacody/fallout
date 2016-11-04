8b549723376d8b326ef62e2b581a9ee724ae070e
Corin Cody <cacody@umich.edu>
2016-11-04 11:12:20 -0400

    changes to make andromeda copy into fallout

--- template.php
+++ template.php
@@ -4,6 +4,6 @@
  * The primary PHP file for this theme.
  */
 
-function andromeda_preprocess_html(&$variables) {
+function fallout_preprocess_html(&$variables) {
   drupal_add_js('https://use.fontawesome.com/971844ebf1.js', array('type' => 'external'));
 }
