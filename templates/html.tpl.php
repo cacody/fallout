ce121413be6563bfaf7fa1fd63d873c9e3772422
Corin Cody <cacody@umich.edu>
2016-11-15 12:01:50 -0500

    adding jquery migrate

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -67,6 +67,7 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js"></script>
   <?php print $scripts; ?>
 </head>
 <body<?php print $body_attributes; ?>>
