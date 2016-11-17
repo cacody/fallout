6afbc35ab2d200b60c1578bffc7c821375009416
Corin Cody <cacody@umich.edu>
2016-11-17 12:58:05 -0500

    remove the forced jq items, start over

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -66,8 +66,7 @@
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 <?php print $scripts; ?>
   
 </head>
